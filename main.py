1a7ca21f8e8b6b198e1de20755d3cc8ac6a56d48
GioJIkia <giorgi.jikiaa@gmail.com>
2026-07-23 19:52:43 +0400

    correct main.py and student_management.py

--- main.py
+++ main.py
@@ -989,7 +989,7 @@ student_management_code = r"""from faker import Faker
 from random import choice
 import json
 import os
-import re 
+import re
 
 # Faker ბიბლიოთეკის შემოტანა ქართული ინიციალებით
 fake = Faker("ka_GE")
@@ -1000,13 +1000,14 @@ if not os.path.exists("students.json"):
         {
             "roll_number": i,
             "name": fake.name(),
-            "grade": choice(["A", "B", "C", "D", "E", "F"])
+            "grade": choice(["A", "B", "C", "D", "E", "F"]),
         }
         for i in range(1, 101)
     ]
     # მონაცემების ჩაწერა json ფაილში
     with open("students.json", "w", encoding="utf-8") as file:
         json.dump(students, file, indent=2, ensure_ascii=False)
+
 
 # სტუდენტის კლასი (OOP)
 class Student:
@@ -1024,7 +1025,7 @@ class Student:
     # სეტერი სახელის ვალიდაციისთვის
     @name.setter
     def name(self, value: str):
-        self._name = validate_name (value)
+        self._name = validate_name(value)
 
     # გეთერი შეფასების ვალიდაციისთვის
     @property
@@ -1036,10 +1037,12 @@ class Student:
     def grade(self, value: str):
         valid_grades = ["A", "B", "C", "D", "E", "F"]
         formatted_grade = str(value).strip().upper()
-        
+
         if formatted_grade not in valid_grades:
-            raise ValueError(f"არასწორი შეფასება! უნდა იყოს ერთ-ერთი შემდეგიდან: {', '.join(valid_grades)}")
-            
+            raise ValueError(
+                f"არასწორი შეფასება! უნდა იყოს ერთ-ერთი შემდეგიდან: {', '.join(valid_grades)}"
+            )
+
         self._grade = formatted_grade
 
     # გეთერი სიითი ნომრის ვალიდაციისთვის
@@ -1053,22 +1056,23 @@ class Student:
         if not (1 <= value <= 200):
             raise ValueError("გთხოვთ შეიყვანოთ ნომერი 1-დან 200-მდე")
         self._roll_number = value
-        
-    # სტუდენტის ობიექტის ტექსტური წარმოდგენა   
+
+    # სტუდენტის ობიექტის ტექსტური წარმოდგენა
     def __str__(self):
         return f"სახელი და გვარი: {self.name}, სიითი ნომერი: {self.roll_number}, ნიშანი: {self.grade}"
+
 
 # სტუდენტების მართვის სისტემის კლასი
 class StudentManagement:
     def __init__(self, json_file="students.json"):
         self.json_file = json_file
         self.students = []
-        #ობიექტის შექმნისთანავე ტვირთავს მონაცემებს JSON-დან
+        # ობიექტის შექმნისთანავე ტვირთავს მონაცემებს JSON-დან
         self.load_from_json()
 
     # JSON ფაილიდან მონაცემების წაკითხვა და Student ობიექტებად ქცევა
     def load_from_json(self):
-    
+
         if not os.path.exists(self.json_file):
             self.students = []
             return
@@ -1079,29 +1083,29 @@ class StudentManagement:
             for student in data:
                 try:
                     self.students.append(
-                    Student(
-                        student["name"],
-                        student["roll_number"],
-                        student["grade"]
-                    )
+                        Student(
+                            student["name"], student["roll_number"], student["grade"]
+                        )
                     )
                 except ValueError as e:
                     print(e)
                     continue
-                    
-    # მიმდინარე სტუდენტების სიის JSON ფაილში შენახვის ფუნქცია 
+
+    # მიმდინარე სტუდენტების სიის JSON ფაილში შენახვის ფუნქცია
     def save_to_json(self):
         data = []
         for student in self.students:
-            data.append({
-                "name": student.name,
-                "roll_number": student.roll_number,
-                "grade": student.grade
-            })
+            data.append(
+                {
+                    "name": student.name,
+                    "roll_number": student.roll_number,
+                    "grade": student.grade,
+                }
+            )
         with open(self.json_file, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=2, ensure_ascii=False)
 
-    #ახალი სტუდენტის დამატების ფუნქცია     
+    # ახალი სტუდენტის დამატების ფუნქცია
     def add_student(self, name: str, roll_number: int, grade: str):
         if not (101 <= roll_number <= 200):
             print("შეცდომა: ახალი სტუდენტის ნომერი უნდა იყოს 101-დან 200-მდე.")
@@ -1115,7 +1119,7 @@ class StudentManagement:
             self.students.append(student)
             self.save_to_json()
             print()
-            print (f"სტუდენტი {name} წარმატებით დამატებულია")
+            print(f"სტუდენტი {name} წარმატებით დამატებულია")
             return True
         except ValueError as e:
             print(f"შეცდომა: {e}")
@@ -1144,38 +1148,46 @@ class StudentManagement:
         student = self.get_student_by_roll_number(roll_number)
         if student:
             formatted_grade = str(grade).strip().upper()
-            
+
             # შემოწმება: ხომ არ არის ახალი ნიშანი იგივე, რაც მიმდინარეა
             if student.grade == formatted_grade:
-                print ()
-                print(f"შეცდომა: სტუდენტს {student.name} უკვე აქვს შეფასება {formatted_grade}! შეფასება ვერ განახლდება.")
-                print ()
+                print()
+                print(
+                    f"შეცდომა: სტუდენტს {student.name} უკვე აქვს შეფასება {formatted_grade}! შეფასება ვერ განახლდება."
+                )
+                print()
                 return False
             try:
                 student.grade = formatted_grade
                 self.save_to_json()
                 print()
-                print(f"შეფასება წარმატებით განახლდა, {student.name}ს შეფასება უკვე არის {student.grade}")
+                print(
+                    f"შეფასება წარმატებით განახლდა, {student.name}ს შეფასება უკვე არის {student.grade}"
+                )
                 return True
             except ValueError:
                 print()
-                print("შეცდომა: გთხოვთ შეიყვანოთ შემდეგი ნიშნებიდან ერთ-ერთი A, B, C, D, E, F")
+                print(
+                    "შეცდომა: გთხოვთ შეიყვანოთ შემდეგი ნიშნებიდან ერთ-ერთი A, B, C, D, E, F"
+                )
                 print()
                 return False
 
         print("სტუდენტი ვერ მოიძებნა.")
         return False
 
+
 # დამხმარე ფუნქცია მომხმარებლისგან მთელი რიცხვის უსაფრთხოდ მისაღებად
 def get_valid_int(prompt):
     while True:
         try:
-            value = int (input(prompt))
+            value = int(input(prompt))
             return value
         except ValueError:
             print()
-            print("შეიყვანეთ მხოლოდ მთელი რიცხვი 1-დან 100-მდე")
+            print("შეიყვანეთ მხოლოდ მთელი რიცხვი")
             print()
+
 
 # სახელისა და გვარის ფორმატისა და სიმბოლოების ვალიდაცია Regex-ის გამოყენებით
 def validate_name(name):
@@ -1189,11 +1201,10 @@ def validate_name(name):
         )
 
     if not (4 <= len(normalized_name) <= 25):
-        raise ValueError(
-            "გთხოვთ შეიყვანოთ სწორი სახელი და გვარი"
-        )
+        raise ValueError("გთხოვთ შეიყვანოთ სწორი სახელი და გვარი")
 
     return normalized_name
+
 
 # პროგრამის მთავარი მენიუ
 def main_menu():
@@ -1222,14 +1233,43 @@ def main_menu():
                     break
                 except ValueError as e:
                     print(f"შეცდომა: {e}")
-                    print()    
-             
-            print()
-            roll_num = get_valid_int("შეიყვანეთ სიის ნომერი: ")
-            
+                    print()
+
             print()
             while True:
-                grade = input("შეიყვანეთ ერთ-ერთი შეფასება (A, B, C, D, E, F): ").strip().upper()
+                roll_num = get_valid_int("შეიყვანეთ სიის ნომერი: ")
+
+                if not roll_num:
+                    print()
+                    print(
+                        "ახალი სტუდენტის სიითი ნომერი შეიძლება იყოს 101-დან 200-მდე..."
+                    )
+                    print()
+                    continue
+
+                if not (101 <= roll_num <= 200):
+                    print()
+                    print(
+                        "ახალი სტუდენტის სიითი ნომერი შეიძლება იყოს 101-დან 200-მდე..."
+                    )
+                    print()
+                    continue
+                if system.get_student_by_roll_number(roll_num):
+                    print()
+                    print(
+                        f"სტუდენტი სიითი ნომრი {roll_num} უკვე არსებობს, გთხოვთ შეიყვანოთ სიითი ნომერი 101-დან 200-მდე."
+                    )
+                    print()
+                    continue
+                break
+
+            print()
+            while True:
+                grade = (
+                    input("შეიყვანეთ ერთ-ერთი შეფასება (A, B, C, D, E, F): ")
+                    .strip()
+                    .upper()
+                )
 
                 if grade not in ["A", "B", "C", "D", "E", "F"]:
                     print()
@@ -1238,17 +1278,17 @@ def main_menu():
                     continue
 
                 break
-                
+
             system.add_student(name, roll_num, grade)
 
         # ყველა სტუდენტის გამოჩენა
         elif choice == "2":
             system.show_all_students()
 
-        #სტუდენტის ძებნა ნომრით
+        # სტუდენტის ძებნა ნომრით
         elif choice == "3":
             roll_num = get_valid_int("შეიყვანეთ საძიებო სტუდენტის სიის ნომერი: ")
-            print ()
+            print()
             student = system.get_student_by_roll_number(roll_num)
             if student:
                 print("ნაპოვნია სტუდენტი მითითებული სიითი ნომრით:")
@@ -1256,30 +1296,41 @@ def main_menu():
                 print()
                 print(student)
             else:
-                print(f"სტუდენტი ნომრით {roll_num} ვერ მოიძებნა, გთხოვთ შეიყვანოთ სიითი ნომერი 1-დან 100-მდე.")
+                print()
+                print(
+                    f"სტუდენტი ნომრით {roll_num} ვერ მოიძებნა, გთხოვთ შეიყვანოთ სიითი ნომერი 1-დან 100-მდე."
+                )
 
         # სტუდენტის შეფასების შეცვლა
         elif choice == "4":
             while True:
-                roll_num = get_valid_int("შეიყვანეთ სტუდენტის სიის ნომერი შეფასების შესაცვლელად: ")
+                roll_num = get_valid_int(
+                    "შეიყვანეთ სტუდენტის სიის ნომერი შეფასების შესაცვლელად: "
+                )
                 student = system.get_student_by_roll_number(roll_num)
                 if student:
                     print()
-                    print(f"თქვენს მიერ არჩეული სტუდენტია: {student.name} (მიმდინარე შეფასება: {student.grade})")
+                    print(
+                        f"თქვენს მიერ არჩეული სტუდენტია: {student.name} (მიმდინარე შეფასება: {student.grade})"
+                    )
                     print()
                     new_grade = input("შეიყვანეთ ახალი შეფასება: ").strip()
                     if system.update_student_grade(roll_num, new_grade):
                         break
                 else:
-                    print(f"სტუდენტი ნომრით {roll_num} ვერ მოიძებნა, გთხოვთ შეიყვანოთ სიითი ნომერი 1-დან 100-მდე.")
+                    print()
+                    print(
+                        f"სტუდენტი ნომრით {roll_num} ვერ მოიძებნა, გთხოვთ შეიყვანოთ სიითი ნომერი 1-დან 100-მდე."
+                    )
                     break
-        
+
         # გამოსვლა პროგრამიდან
         elif choice == "5":
             print("პროგრამა დასრულებულია. ნახვამდის!")
             break
         else:
             print("თქვენ არასწორად აირჩიეთ. გთხოვთ აირჩიოთ ციფრი 1-დან 5-მდე.")
+
 
 # პროგრამის გაშვება
 main_menu()
